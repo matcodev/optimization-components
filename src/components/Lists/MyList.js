1c33868c5beb850cb13c6496d7863570bea2875d
mxespin <matias@clippercapital.cl>
2022-02-21 23:17:31 -0300

    first component rendering with memo

---
+++ src/components/Lists/MyList.js
@@ -0,0 +1,48 @@
+import { isEqual } from 'lodash'
+import { memo } from 'react'
+
+// const Li = memo(({ fullname }) => {
+//   console.log(`renderizando ${fullname}`)
+//   return (
+//     <li>
+//       {fullname}
+//     </li>
+//   )
+// })
+
+const Li = memo(({ children }) => {
+  console.log(`Renderizando ${children}`)
+  return (
+    <li>
+      {children}
+    </li>
+  )
+}, isEqual)
+
+const MyList = ({ data }) => {
+  console.log('Renderizando Lista')
+  return (
+    <ul>
+      {data.map(x => 
+        <Li key={x.name + x.lastname}>
+          {x.name} {x.lastname}
+        </Li> )}
+    </ul>
+  )
+}
+
+// const MyList = ({ data }) => {
+//   console.log('renderizando lista')
+//   return (
+//     <ul>
+//       {data.map(x =>
+//         <Li
+//           key={x.name + x.lastname}
+//           fullname={`${x.name} ${x.lastname}`}
+//         />
+//       )}
+//     </ul>
+//   )
+// }
+
+export default memo(MyList)
